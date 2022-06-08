5f23ae0b0997eeb70dea190b6937d86bd7c007e8
jesielviana <jesielviana@pm.me>
2022-06-08 13:56:20 -0300

    config runners

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,8 @@ echo "Running npm install"
 npm install
 echo "Running pm2 stop oasisbr-api"
 pm2 stop oasisbr-api
+echo "Copy local configs"
+cp /usr/local/configs/oasisbr-api/.env.local  .env.local
 echo "npm run deploy:prod"
 npm run deploy:prod
 echo "finished!"
