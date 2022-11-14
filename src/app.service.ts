cbd6dd5ccc2400ed0e6486bbc71fcb1ebca75f4d
Jesiel Viana <jesielviana@proton.me>
2022-11-14 14:08:13 -0300

    renomeando runner de producao

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'OasisBr API is Online: Version: 1.10.1';
+    return 'Oasisbr API is Online: Version: 1.10.2';
   }
 }
