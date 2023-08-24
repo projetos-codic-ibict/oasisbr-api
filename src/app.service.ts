cbbbc0279d80605842984e781d521a44685176dc
Jesiel Viana <jesielviana@proton.me>
2023-08-24 09:31:19 -0300

    atualização da versãp

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Oasisbr API is Online: Version: 1.10.2';
+    return 'Oasisbr API is Online: Version: 2.1.0';
   }
 }
