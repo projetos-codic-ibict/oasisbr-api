66cc38d3a1f58de1f2e3e52802f6febdeab5149f
Jesiel Viana <jesielviana@proton.me>
2023-11-22 15:05:32 -0300

    atualizando script de deploy para pegar variaveis do env do github

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ npm install
 echo "Running pm2 stop oasisbr-api"
 pm2 stop oasisbr-api
 echo "Copy local configs"
-cp /usr/local/configs/oasisbr-api/.env.local  .env.local
+cp .env  .env.local
 echo "npm run deploy:prod"
 npm run deploy:prod
 echo "finished!"
