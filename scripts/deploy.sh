a448d9ff63d1051286937aafc97f38b9d03fadfc
Jesiel Viana <jesielviana@proton.me>
2024-05-16 13:13:22 -0300

    trocando yarn pelo npm

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,14 +2,14 @@
 set -e
 echo "Versão do Node"
 node -v
-echo "Running yarn install"
-yarn
+echo "Running npm install"
+npm install
 echo "Running pm2 stop all"
 pm2 stop all || true
 echo "copy env configs"
 cp .env.example  .env
-echo "yarn deploy"
-yarn deploy
+echo "npm run deploy"
+npm run deploy
 echo "pm2 save"
 pm2 save
 echo "finished!"
