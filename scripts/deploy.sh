a0d42ee6ca4cdc638111f3d56849b24776011d3e
Jesiel Viana <jesielviana@proton.me>
2024-05-16 14:44:27 -0300

    Voltando com yarn

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,17 +2,14 @@
 set -e
 echo "Versão do Node"
 node -v
-npm install pm2 -g
-echo "Versão do PM2"
-pm2 -v
-echo "Running npm install"
-npm install
+echo "Running yarn install"
+yarn
 echo "Running pm2 stop all"
 pm2 stop all || true
-echo "copy .env.example para .env"
+echo "copy env configs"
 cp .env.example  .env
-echo "npm run deploy"
-npm run deploy
+echo "yarn deploy"
+yarn deploy
 echo "pm2 save"
 pm2 save
 echo "finished!"
