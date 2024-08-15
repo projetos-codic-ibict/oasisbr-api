3bbc522925b8a01acf945effc80814f61787656c
Jesiel Viana <jesielviana@proton.me>
2024-08-15 19:45:50 -0300

    alterando url da doc

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
+import { RequestMethod } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as helmet from 'helmet';
-
-import { RequestMethod } from '@nestjs/common';
 import { AppModule } from './app.module';
-
-console.log('process.env.NODE_ENV', process.env.NODE_ENV);
-console.log('process.env.DATABASE_NAME', process.env.DATABASE_NAME);
-console.log('process.env.DATABASE_USER', process.env.DATABASE_USER);
-console.log('process.env.HARVESTER_API_USERNAME', process.env.HARVESTER_API_USERNAME);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {});
@@ -27,7 +21,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/v1/doc', app, document);
+  SwaggerModule.setup('/', app, document);
   await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
--- src/networks/networks.controller.ts
+++ src/networks/networks.controller.ts
@@ -1,20 +1,31 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Controller, Get, Param, Query } from '@nestjs/common';
-import { ApiResponse } from '@nestjs/swagger';
+import { ApiProperty, ApiResponse } from '@nestjs/swagger';
 import { Network } from '@prisma/client';
 import { NetworksService } from './networks.service';
 
 class NetworkType implements Network {
+  @ApiProperty()
   id: number;
+  @ApiProperty()
   acronym: string;
+  @ApiProperty()
   issn: string;
+  @ApiProperty()
   name: string;
+  @ApiProperty()
   institution: string;
+  @ApiProperty()
   sourceType: string;
+  @ApiProperty()
   email: string;
+  @ApiProperty()
   sourceUrl: string;
+  @ApiProperty()
   validSize: number;
+  @ApiProperty()
   uf: string;
+  @ApiProperty()
   updatedAt: Date;
 }
 
@@ -23,7 +34,7 @@ export class NetworksController {
   constructor(private readonly networksService: NetworksService) {}
 
   @Get()
-  @ApiResponse({ status: 200, type: '', isArray: true })
+  @ApiResponse({ status: 200, type: NetworkType, isArray: true })
   async findAll(@Query('sourceType') sourceType, @Query('name') name): Promise<Network[]> {
     console.log('sourceType', sourceType);
     if (sourceType) {
